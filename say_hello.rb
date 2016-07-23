de4704546501f2d1ff54b0f7ef2b27dee6f202a0
Alyssa Foxx <alyssamariefoxx@gmail.com>
2016-07-23 02:31:35 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,4 @@
-# Build your say_hello method here
+# uild your say_hello method heredef greeting(name="Ruby programmer", language="Ruby")
+def say_hello (name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
